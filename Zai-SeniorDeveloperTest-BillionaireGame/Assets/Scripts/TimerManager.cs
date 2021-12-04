79afb166eb23ff51dcc9ce78370891c7c388047d
Zyanide1504 <42516755+Zyanide1504@users.noreply.github.com>
2021-12-04 15:45:10 +0700

    little Change
    Add Thai Font fix
    Add Notification

--- Zai-SeniorDeveloperTest-BillionaireGame/Assets/Scripts/TimerManager.cs
+++ Zai-SeniorDeveloperTest-BillionaireGame/Assets/Scripts/TimerManager.cs
@@ -2,16 +2,50 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using Unity.Notifications.Android;
+
 
 public class TimerManager : MonoBehaviour
 {
     public TimerInput_panel hourInput;
     public TimerInput_panel minuteInput;
     private DateTime CurrentTimer;
+    private AndroidNotificationChannel defaultNotificationChanel;
+
+    public void Start()
+    {
+
+#if UNITY_ANDROID
+        defaultNotificationChanel = new AndroidNotificationChannel() 
+        {
+            Id = "default_channel",
+            Name = "Default Channel",
+            Description = "For Generic notification",
+            Importance = Importance.Default,
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(defaultNotificationChanel);
+#endif
+
+    }
 
     public void SaveTimer() 
     {
         CurrentTimer = DateTime.Parse(hourInput.current_input + ":" + minuteInput.current_input);
+
+#if UNITY_ANDROID
+        AndroidNotification notification = new AndroidNotification()
+        {
+            Title = "Test Notification!!!",
+            Text = "This is a test notification!!!",
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = CurrentTimer,
+        };
+
+        AndroidNotificationCenter.SendNotification(notification, "default_channel");
+#endif
+
     }
 
     public void StartGame() 
---
+++ Zai-SeniorDeveloperTest-BillionaireGame/Assets/Scripts/UI_Scripts/Text_ForThaiFont.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Text_ForThaiFont : MonoBehaviour
+{
+    private Text Thai_text;
+
+    public void Start()
+    {
+        Thai_text = this.GetComponent<Text>();
+        Thai_text.text = ThaiFontAdjuster.Adjust(Thai_text.text);
+    }
+
+}
