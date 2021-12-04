58c9e91c5a17d55a0ac08ec0b6ccc26510316952
Zyanide1504 <42516755+Zyanide1504@users.noreply.github.com>
2021-12-04 14:20:44 +0700

    add Font

---
+++ Zai-SeniorDeveloperTest-BillionaireGame/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    public TimerInput_panel hourInput;
+    public TimerInput_panel minuteInput;
+    private DateTime CurrentTimer;
+
+    public void SaveTimer() 
+    {
+        CurrentTimer = DateTime.Parse(hourInput.current_input + ":" + minuteInput.current_input);
+    }
+
+    public void StartGame() 
+    {
+        if (DateTime.Now > CurrentTimer)
+        {
+
+
+        }
+        else 
+        {
+        
+        }
+    
+    }
+}
