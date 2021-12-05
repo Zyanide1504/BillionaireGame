430b168047d897012172f59a6e757b20c994085a
Zyanide1504 <42516755+Zyanide1504@users.noreply.github.com>
2021-12-05 10:37:06 +0700

    Start dev Gameplay  scene

---
+++ Zai-SeniorDeveloperTest-BillionaireGame/Assets/Scripts/Card/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [Header("Card Info")]
+    public string card_Name;
+    public string card_Description;
+
+    [Header("Card Front & Back")]
+    public GameObject card_Front;
+    public GameObject card_Back;
+
+    bool cardBackIsActive;
+
+    
+
+    Vector3 card_rotation;
+
+
+    IEnumerator CalculateFlip() 
+    {
+    
+    }
+
+
+
+}
